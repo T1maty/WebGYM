10b277cb02c68947ec6c4308a986b9ed0f0d9107
Solo <101876583+T1maty@users.noreply.github.com>
2023-01-28 19:51:05 +0200

    Add Options, Add Interface Service and add NpModels

---
+++ WebGYM/Services/Interfaces/IActivitiesService.cs
@@ -0,0 +1,11 @@
+﻿using WebGYM.Models;
+
+namespace WebGYM.Services.Interfaces
+{
+    public interface IActivitiesService
+    {
+        public Activities AddActivities(Activities activities);
+
+        public Activities GetActivities(Activities activities);
+    }
+}
