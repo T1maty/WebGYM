c915d632b1f91c28c40f059ec276f2d0ca883a69
Solo <101876583+T1maty@users.noreply.github.com>
2023-01-28 18:15:38 +0200

    Add BaseObject

--- WebGYM/Models/User.cs
+++ WebGYM/Models/User.cs
@@ -1,11 +1,11 @@
 ﻿namespace WebGYM.Models
 {
-    public class User
+    public class User : BaseObject
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public  string Email { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public  string? Email { get; set; }
         public int UserId { get;  set; }
         
     }
