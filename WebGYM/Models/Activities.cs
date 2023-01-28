83757d3f5e2b490b6a4a79f58335acc1fb78264e
Solo <101876583+T1maty@users.noreply.github.com>
2023-01-28 18:57:54 +0200

    Create Activities

---
+++ WebGYM/Models/Activities.cs
@@ -0,0 +1,10 @@
+﻿namespace WebGYM.Models
+{
+    public class Activities : BaseObject
+    {
+        public string? Category { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string? Day { get; set; }
+
+    }
+}
