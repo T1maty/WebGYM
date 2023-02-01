933139908644730ce95a5d0b4eed4851c79ec71c
Solo <101876583+T1maty@users.noreply.github.com>
2023-02-01 23:07:01 +0200

    Mapping in UserDetailsVm

---
+++ WebGYM.Application/Gym/Queries/UserDetailsVm.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebGYM.Application.Common.Mappings;
+using WebGYM.Domain;
+
+namespace WebGYM.Application.Gym.Queries
+{
+    public class UserDetailsVm : IMapWith<User>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public int UserId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<User, UserDetailsVm>()
+                .ForMember(userVm => userVm.Email,
+                opt => opt.MapFrom(user => user.Email))
+                .ForMember(userVm => userVm.FirstName,
+                opt => opt.MapFrom(user => user.FirstName))
+                .ForMember(userVm => userVm.UserId,
+                opt => opt.MapFrom(user => user.UserId))
+                .ForMember(userVm => userVm.LastName,
+                opt => opt.MapFrom(user => user.LastName));
+        }
+    }
+}
