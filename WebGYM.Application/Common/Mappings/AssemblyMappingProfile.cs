c5c4f4682c514cd56ebcac31dd632078bea1d81a
Solo <101876583+T1maty@users.noreply.github.com>
2023-02-01 22:06:17 +0200

    Create Automapper

--- WebGYM.Application/Common/Mappings/AssemblyMappingProfile.cs
+++ WebGYM.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -12,6 +12,22 @@ namespace WebGYM.Application.Common.Mappings
     {
         public AssemblyMappingProfile(Assembly assembly) =>
             ApplyMappingsFromAssembly(assembly);
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType && 
+                i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
     
     }
 }
