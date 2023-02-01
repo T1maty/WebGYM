c77cc12686ebd121df57c9b9788d88fed457708e
T1maty <timamy666@gmail.com>
2023-02-01 21:45:10 +0300

    Create Assembly

---
+++ WebGYM.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebGYM.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+    
+    }
+}
