010344f0216a21945f03b021244f1a9c76fc93b6
Solo <101876583+T1maty@users.noreply.github.com>
2023-02-01 23:19:10 +0200

    Create DI

---
+++ WebGYM.Persistance/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WebGYM.Application.Interfaces;
+
+namespace WebGYM.Persistance
+{
+    public static  class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence( this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DefaultConnection"];
+            services.AddDbContext<WebGymDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.AddScoped<IWebGymDbContext>(provider => (IWebGymDbContext)provider.GetService<WebGymDbContext>());
+            return services;
+
+        }
+    }
+}
