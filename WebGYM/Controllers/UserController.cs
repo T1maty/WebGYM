a52ef9d4517f1ecf8a0ef5fc624122102cc399f9
T1maty <timamy666@gmail.com>
2023-02-06 21:48:14 +0300

    Add xml to Controller

--- WebGYM/Controllers/UserController.cs
+++ WebGYM/Controllers/UserController.cs
@@ -16,11 +16,37 @@ namespace WebGYM.Controllers
         {
             userService = _userService;
         }
+
+        /// <summary>
+        /// User registers for a subscription
+        /// </summary>
+        /// <remarks>
+        /// Request example
+        /// <code>
+        /// POST /api/User
+        /// {
+        /// firstName: Nakamoto,
+        /// lastName: Satoshi,
+        ///  email: ftomi809@gmail.com,
+        /// }
+        /// </code>
+        /// </remarks>
+        /// 
+        /// <param name="user">Sign Up for a gym membership </param>
+        /// <returns>Nothing</returns>
+        /// <response code="200">Successfully sign!</response>
+
         [HttpPost]
         public User AddUser(User user)
         {
             return userService.AddUser(user);
         }
+        /// <summary>
+        /// Update User Data
+        /// Required fields are first name, last name and phone number
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         [HttpPut]
         public User UpdateUser(User user)
         {
@@ -32,6 +58,7 @@ namespace WebGYM.Controllers
         /// </summary>
         /// <param name="id">User id</param>
         /// <returns>Result of deletion</returns>
+        ///  /// <response code="200">Successfully Log out!</response>
         [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
