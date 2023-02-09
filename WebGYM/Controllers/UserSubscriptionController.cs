f8d6dad56185895ffefa063b8cadd2f9c1b5b0e5
Solo <101876583+T1maty@users.noreply.github.com>
2023-02-09 20:58:04 +0200

    Create POST in new Controller

--- WebGYM/Controllers/UserSubscriptionController.cs
+++ WebGYM/Controllers/UserSubscriptionController.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models.User;
+using WebGYM.Application.Gym.Commands.User.CreateUser;
 
 namespace WebAPI.Controllers
 {
@@ -16,6 +18,22 @@ namespace WebAPI.Controllers
         {
             _mapper = mapper;
             _mediator = mediator;
+        }
+
+        [HttpPost("create-subscription")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> CreateSubscription([FromBody]  CreateUserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+            var command = _mapper.Map<CreateUserCommand>(model);
+            await _mediator.Send(command);
+
+            return Ok();
         }
     }
 }
--- WebGYM/Models/User/CreateUserModel.cs
+++ WebGYM/Models/User/CreateUserModel.cs
@@ -6,6 +6,10 @@ namespace WebAPI.Models.User
 {
     public class CreateUserModel : IMappable
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public void Mapping(Profile profile)
         {
             profile.CreateMap<CreateUserModel, CreateUserCommand>();
