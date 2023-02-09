9332a961a104ffc4f17390c43f3c00656729099f
Solo <101876583+T1maty@users.noreply.github.com>
2023-02-09 20:51:52 +0200

    Create Controller and ad CreateUserModel

---
+++ WebGYM/Controllers/UserSubscriptionController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("user(notauth)")]
+    public class UserSubscriptionController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        public UserSubscriptionController(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+    }
+}
---
+++ WebGYM/Models/User/CreateUserModel.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebGYM.Application.Common.Mappings;
+using WebGYM.Application.Gym.Commands.User.CreateUser;
+
+namespace WebAPI.Models.User
+{
+    public class CreateUserModel : IMappable
+    {
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateUserModel, CreateUserCommand>();
+        }
+    }
+}
