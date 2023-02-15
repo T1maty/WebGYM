428da91fc23961b911503e3d686eaee383bd4005
T1maty <timamy666@gmail.com>
2023-02-15 19:53:53 +0300

    Add Logging user, and add new Service

---
+++ WebGYM.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebGYM.Application.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, 
+            RequestHandlerDelegate<TResponse> next)
+        {
+            var requestName = typeof(TRequest).Name;
+            Log.Information(" User Request: {Name} {@UserId} {@Request}",
+                requestName, userId, request);
+
+            var response = await next();
+            return response;
+        }
+    }
+}
---
+++ WebGYM/Service/CurrentUserService.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using WebGYM.Application.Interfaces;
+
+namespace WebAPI.Service
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public Guid UserId
+        {
+            get
+            {
+                var id = _contextAccessor.HttpContext?.User?
+                    .FindFirstValue(ClaimTypes.NameIdentifier);
+
+                return string.IsNullOrEmpty(id) ? Guid.Empty : Guid.Parse(id);
+            }
+        }
+       
+    }
+}
